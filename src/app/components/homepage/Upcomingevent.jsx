7b8f761b1d09c35e9585adaccfb5add31b0b0068
navedcodelinear <naved@codelinear.com>
2025-06-12 09:59:25 +0530

    changes

--- src/app/components/homepage/Upcomingevent.jsx
+++ src/app/components/homepage/Upcomingevent.jsx
@@ -12,18 +12,18 @@ export default function UpcomingEvents() {
           height={100}
           className="lg:w-[50%]  w-full object-cover"
         />
+
         <div className='lg:w-[50%] w-full  relative'>
-            <div className='w-full absolute left-[-70px] bg-white pb-10 pl-20 flex flex-col gap-5'>
+            <div className='w-full absolute lg:left-[-70px] bg-white pb-10 px-5 lg:pr-0 mt-5 lg:mt-0 lg:pl-20 flex flex-col gap-5'>
               <h1 className='text-[#515151] text-[46px] font-semibold leading-[100%]'>Upcoming Events</h1>
               <p className='text-[14px] leading-[140%]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.</p>
             </div>
             
-            <div className='bg-[#F1ECE0] py-10 mt-[165px] px-10'>
+            <div className='bg-[#F1ECE0] py-10 mt-[200px] lg:mt-[165px] px-10'>
             <h1 className='text-[#515151] text-[26px] font-semibold leading-[100%] mb-5'>05.17.25 // RIMS Orlando</h1>
             <p className='text-[#515151] text-[14px] font-semibold leading-[100%] mb-4'>IW and ProCare Connect Demo | 9:00 am - 12:30 pm | Room 1023</p>
             <p className='text-[#696969] text-[14px]  leading-[140%]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.</p>
             </div>
-            
         </div>
      
     </section>
